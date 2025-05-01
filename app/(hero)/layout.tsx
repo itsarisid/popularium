0d7219c05f7dda02cd9d970a7666eeb2d9004ed8
Sajid Khan <khanalex301@gmail.com>
2025-05-02 00:04:35 +0300

    Layout with new css

--- app/(hero)/layout.tsx
+++ app/(hero)/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
-import "@/styles/theme.css";
+import "@/styles/index.css";
 import ThemeProvider from "@/theme/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Carbon",
-  description: "New generation of carbon accounting",
+  title: "Popularium",
+  description: "New generation of carbon coding",
 };
 
 export default function RootLayout({
