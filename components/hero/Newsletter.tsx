b760e543828b5926d96dfaf49f007553276f76f7
Sajid Khan <khanalex301@gmail.com>
2025-05-02 13:09:40 +0300

    news letter

--- components/hero/Newsletter.tsx
+++ components/hero/Newsletter.tsx
@@ -12,7 +12,7 @@ export const Newsletter = () => {
     <section id="newsletter">
       <hr className="w-11/12 mx-auto" />
 
-      <div className="container py-24 sm:py-32">
+      <div className="container mx-auto py-24 sm:py-32">
         <h3 className="text-center text-4xl md:text-5xl font-bold">
           Join Our Daily{" "}
           <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
