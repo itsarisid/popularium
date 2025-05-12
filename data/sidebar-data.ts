e0c585498c093c2d7c5035234a45d3919ad6c531
Sajid Khan <khanalex301@gmail.com>
2025-05-12 13:55:34 +0300

    Menu Orgnize

--- data/sidebar-data.ts
+++ data/sidebar-data.ts
@@ -1,59 +1,94 @@
 import { SidebarData } from '@/models/types'
-  import { ActivitySquare, Fingerprint, Gauge, LayoutDashboard, Lock, ShieldCheck, SnailIcon, SquareUserRound } from 'lucide-react'
-  
-  export const sidebarData: SidebarData = {
-    user: {
+import { ActivitySquare, Fingerprint, Gauge, LayoutDashboard, Lock, LogInIcon, ShieldCheck, SnailIcon, SquareUserRound, User2Icon } from 'lucide-react'
+
+export const sidebarData: SidebarData = {
+  user: {
+    name: 'Sajid Khan',
+    email: 'sajid@example.com',
+    avatar: '#',
+  },
+  teams: [
+    {
       name: 'Sajid Khan',
-      email: 'sajid@example.com',
-      avatar: '#',
+      logo: SnailIcon,
+      plan: '',
+    }
+  ],
+  navGroups: [
+    {
+      title: 'General',
+      items: [
+        {
+          title: 'Dashboard',
+          url: '/dashboard',
+          icon: Gauge,
+        },
+        // {
+        //   title: 'Users',
+        //   url: '/users',
+        //   icon: SquareUserRound,
+        // },
+        // {
+        //   title: 'Roles',
+        //   url: '/roles',
+        //   icon: ShieldCheck,
+        // },
+        // {
+        //   title: 'Permissions',
+        //   url: '/permissions',
+        //   icon: Lock,
+        // },
+        // {
+        //   title: 'Activity Logs',
+        //   url: '/activity',
+        //   icon: ActivitySquare,
+        // },
+        // {
+        //   title: 'Dashboard',
+        //   url: '/user-dashboard',
+        //   icon: LayoutDashboard,
+        // },
+        // {
+        //   title: 'Role Assignment',
+        //   url: '/user-roles',
+        //   icon: Fingerprint,
+        // },
+      ],
+
     },
-    teams: [
-      {
-        name: 'Sajid Khan',
-        logo: SnailIcon,
-        plan: '',
-      }
-    ],
-    navGroups: [
-      {
-        title: 'General',
-        items: [
-          {
-            title: 'Dashboard',
-            url: '/dashboard',
-            icon: Gauge,
-          },
-          {
-            title: 'Users',
-            url: '/users',
-            icon: SquareUserRound,
-          },
-          {
-            title: 'Roles',
-            url: '/roles',
-            icon: ShieldCheck,
-          },
-          {
-            title: 'Permissions',
-            url: '/permissions',
-            icon: Lock,
-          },
-          {
-            title: 'Activity Logs',
-            url: '/activity',
-            icon: ActivitySquare,
-          },
-          {
-            title: 'Dashboard',
-            url: '/user-dashboard',
-            icon: LayoutDashboard,
-          },
-          {
-            title: 'Role Assignment',
-            url: '/user-roles',
-            icon: Fingerprint,
-          },
-        ],
-      },
-    ],
-  }
+    {
+      title: 'Settings',
+      items: [
+        {
+          title: 'Users',
+          icon: User2Icon,
+          items: [
+            {
+              title: 'Dashboard',
+              url: '/user-dashboard',
+            },
+            {
+              title: 'Users',
+              url: '/users',
+            },
+            {
+              title: 'Roles',
+              url: '/roles',
+            },
+            {
+              title: 'Role Assignment',
+              url: '/roles',
+            },
+            {
+              title: 'Permissions',
+              url: '/user-roles',
+            },
+            {
+              title: 'Activity Logs',
+              url: '/activity',
+            },
+          ],
+        }]
+    }
+  ],
+}
